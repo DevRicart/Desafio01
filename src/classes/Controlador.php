cd8ef9f8aa69cb630fa24b07225c1874bec9dfc3
Ricardo Gonçalves <104541413+DevRicart@users.noreply.github.com>
2024-08-23 15:30:28 -0300

    Tira comentários

--- src/classes/Controlador.php
+++ src/classes/Controlador.php
@@ -14,37 +14,33 @@ class Controlador
   public function findMercadoria($busca) {
     $mercadorias = $this->getMercadorias();
 
-    if (array_key_exists($busca, $mercadorias)) { //TODO: verificar a existencia da chave $busca no array $mercadorias. (FOOOOOOOI)
+    if (array_key_exists($busca, $mercadorias)) {
       return $mercadorias[$busca];
-       
     } else {
       $content = file_get_contents(__DIR__ . '/../views/404.html');
-
-      require(__DIR__ . '/../views/layout.php'); // aqui cabe adicionar o 404 (fooooi)
+      require(__DIR__ . '/../views/layout.php');
     }
   }
    
   public function render($mercadoria) {
- // nao precisa mais (feito)
- // remover esse if todo (feito)
       $mercadoria = new Mercadoria($mercadoria);
       ob_start();
       require(__DIR__ . '/../views/dados-mercadoria.php');
       $content = ob_get_clean();
-      
+
       require(__DIR__ . '/../views/layout.php');
   }
   
   public function run() {
-    if (isset($_GET['mercadoria'])) { // show (show)
+    if (isset($_GET['mercadoria'])) {
       $mercadoria = $this->findMercadoria($_GET['mercadoria']);
       $this->render($mercadoria); 
     }
-    else if ($_SERVER['REQUEST_URI'] == "/") { // caso a url não seja a padrão (somento o dominio)
+    else if ($_SERVER['REQUEST_URI'] == "/") {
       $content = file_get_contents(__DIR__ . '/../views/selecione.html');
       require(__DIR__ . '/../views/layout.php');
     } 
-    else { // show (show)
+    else {
       $content = file_get_contents(__DIR__ . '/../views/404.html');
       require(__DIR__ . '/../views/layout.php');
     }
